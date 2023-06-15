3bbfa5e03739d5a21dda82217e27b7ac9bb371bc
valderlan jose da silva <105988303+valderlanjs@users.noreply.github.com>
2023-06-15 01:22:21 -0300

    resolvido o problema

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const passwordInput = document.querySelector(".pass-field input");
 const eyeIcon = document.querySelector(".pass-field i");
-const requirementList = document.querySelector(".requirement-list li");
+const requirementList = document.querySelectorAll(".requirement-list li");
 
 //uma matriz de senha exigida com correspondente
 //expressão regular e índice do item da lista de requisitos
